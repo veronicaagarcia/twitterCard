b3b1d0888f29b74fc3be6cf811512243116f9e8b
Veronica Garcia <veroagarcia90@gmail.com>
2023-11-14 18:24:01 -0300

    still try to publish

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/{your-repo-name}", plugins: [react()],
 })
