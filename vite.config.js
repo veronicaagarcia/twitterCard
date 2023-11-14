a647f852aab0f827593be345b8fd071511a20163
Veronica Garcia <veroagarcia90@gmail.com>
2023-11-14 18:59:51 -0300

    still

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/{your-repo-name}", plugins: [react()],
+  // base: "/{your-repo-name}", plugins: [react()],
+  plugins: [react()],
 })
